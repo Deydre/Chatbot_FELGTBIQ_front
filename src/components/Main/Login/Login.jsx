268640ff48dc791b54d2e8132cd64e86409d62bb
Deydre <deydrealonso@gmail.com>
2024-12-05 20:04:22 +0100

    architecture of components and sass

---
+++ src/components/Main/Login/Login.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import FormLogin from './FormLogin/FormLogin';
+
+const Login = () => {
+  return <section>
+    <FormLogin></FormLogin>
+  </section>;
+};
+
+export default Login;
