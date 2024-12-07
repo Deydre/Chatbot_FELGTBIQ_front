a28556830dc8043bf0449f110c53c55850a03509
Hans <alvarohansorellana@gmail.com>
2024-12-07 18:29:09 +0100

    working chatBot with Form

--- src/components/Main/Home/Home.jsx
+++ src/components/Main/Home/Home.jsx
@@ -6,23 +6,29 @@ const Home = () => {
   const [formData, setFormData] = useState(null); // Datos del formulario
   const [isSubmitted, setIsSubmitted] = useState(false); // Estado del formulario
   const [userType, setUserType] = useState(""); // "sociosanitario" o "no sociosanitario"
+  const updateUserType = (data) => {
+    setUserType(data)
+  };
+  const updateIsSubmitted = (data) => {
+    setIsSubmitted(data)
+  };
 
   // Maneja el envío del formulario
-  const handleFormSubmit = (data, type) => {
-    setFormData(data); // Guarda los datos del formulario
-    setUserType(type); // Guarda el tipo de usuario (sociosanitario o no)
-    setIsSubmitted(true); // Muestra el chatbot
-  };
+  // const handleFormSubmit = (data, type) => {
+  //   setFormData(data); // Guarda los datos del formulario
+  //   setUserType(type); // Guarda el tipo de usuario (sociosanitario o no)
+  //   setIsSubmitted(true); // Muestra el chatbot
+  // };
 
   return (
     <section>
       {!isSubmitted ? (
         // Muestra el formulario
-        <Formulario onSubmit={handleFormSubmit} />
+        <Formulario updateIsSubmitted = {updateIsSubmitted} updateUserType = {updateUserType} userType={userType}/>
       ) : (
         // Muestra el chatbot después de enviar el formulario
         <Chatbot userType={userType} formData={formData} />
-      )}  
+      )}
     </section>
   );
 };
