adefc3f7f20a5d67080e14ead0736fbc854e2a19
RubenC2 <rubenct1993@gmail.com>
2024-12-10 18:00:55 +0100

    loginOKcambioFondo

--- src/services/formLoginAdmin.js
+++ src/services/formLoginAdmin.js
@@ -6,7 +6,7 @@ const urlGetAmin = 'https://chatbot-felgtbiq-back.onrender.com/api/admin/me';
 // Función para el login
 export const fetchHandleLogin = async (email, password) => {
   try {
-    const response = await axios.post(urlLogin, email, password, { 
+    const response = await axios.post(urlLogin, { email, password }, { 
       headers: {
         'Content-Type': 'application/json',
       },
@@ -15,19 +15,23 @@ export const fetchHandleLogin = async (email, password) => {
     response ? console.log(response) : console.log("")
     return response; 
   } catch (error) {
-    throw new Error("Error: " + error.message);
+    throw new Error("Error fetchLogin: " + error.message);
   }
 };
 
 // Función para obtener los datos de admin cuando se autentica
-export const getAdminData = async () => {
-  try {
-    const response = await axios('https://chatbot-felgtbiq-back.onrender.com/api/admin/me', {
-      withCredentials: true,
-    });
-    return response;
-  } catch (error) {
-    throw new Error("Error al obtener datos estadísticos " + error.message);
-  }
-};
+// export const getAdminData = async () => {
+//   try {
+//     const response = await axios.post(urlGetAmin, {
+//         headers: {
+//             'Content-Type': 'application/json',
+//           },
+//     });
+
+//     response ? console.log(response) : console.log("")
+//     return response;
+//   } catch (error) {
+//     throw new Error("Error fetch adminData " + error.message);
+//   }
+// };
 
